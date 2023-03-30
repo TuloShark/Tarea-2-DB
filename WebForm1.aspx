918136dc409df784299aab47cb6b7b534a3d5ae1
Wander Jimenez Calvo <86485839+TuloShark@users.noreply.github.com>
2023-03-29 18:29:02 -0600

    StoredProcedure de XML completamente funcional / Form de Usuario
    Se adjunta el form completo y funcional del usuario, junto a la StoredProcedure 100% funcional (le hacia falta la lectura de la tabla Articulo)

---
+++ WebForm1.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Usuario.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 226px;
+            width: 511px;
+            background-color: #FFFFFF;
+        }
+        .auto-style2 {
+            margin-left: 60px;
+            margin-top: 0px;
+        }
+        .auto-style3 {
+            margin-left: 60px;
+            margin-top: 0px;
+            margin-bottom: 0px;
+        }
+        .auto-style4 {
+            margin-left: 361px;
+            margin-top: 16px;
+            margin-bottom: 0px;
+        }
+        .auto-style5 {
+            font-size: xx-large;
+        }
+        .auto-style6 {
+            color: #FF0000;
+        }
+    </style>
+</head>
+<body style="height: 226px; width: 511px">
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <strong><span class="auto-style5">&nbsp;USER NAME : </span></strong>
+            <asp:TextBox ID="Nombre" runat="server" CssClass="auto-style3" Width="250px"></asp:TextBox>
+&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="Nombre" CssClass="auto-style6" ErrorMessage="Se requiere un UserName"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            <strong><span class="auto-style5">&nbsp;PASSWORD :</span></strong><asp:TextBox ID="Contrasena" runat="server" CssClass="auto-style2" TextMode="Password" Width="250px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="Contrasena" CssClass="auto-style6" ErrorMessage="Se requiere un Password"></asp:RequiredFieldValidator>
+            <br />
+            <br />
+            <asp:Button ID="Aceptar" runat="server" CssClass="auto-style4" OnClick="Button1_Click" Text="ACEPTAR" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Usuario
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Nombre;
+
+        /// <summary>
+        /// Control RequiredFieldValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+
+        /// <summary>
+        /// Control Contrasena.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Contrasena;
+
+        /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
+        /// Control Aceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Aceptar;
+    }
+}
